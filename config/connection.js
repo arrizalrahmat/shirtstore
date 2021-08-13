913c5854286e783f406cc8a4fc2b6ea510647a02
arrizalrahmat <arrizalrahmat@gmail.com>
2021-08-13 11:29:06 +0700

    bebek

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,11 @@
-const { Client } = require('pg');
+const { Pool } = require('pg')
 
-const client = new Client({
-    host: 'ec2-34-194-14-176.compute-1.amazonaws.com',
-    port: 5432,
-    database: 'dddc5slfjk0sj',
-    user: 'lpaguthtnfozff',
-    password: '89393c66e789c2c7954efde235a9a64a8a9964fa21dde562a69417346d48c1f5',
-    connectionTimeoutMillis: 5000,
-    idleTimeoutMillis: 2000,
-    max: 200,
-    ssl: {
-        require: true,
-        rejectUnauthorized: false
-    }
-});
+const pool = new Pool({
+  connectionString : process.env.DATABASE_URL,
+  ssl: {
+    require: true,
+    rejectUnauthorized: false
+  }
+})
 
-client.connect()
-
-module.exports = client;
+module.exports = pool;
