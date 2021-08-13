107169c6c00e12021cb56bca56af8616a1a39329
arrizalrahmat <arrizalrahmat@gmail.com>
2021-08-13 10:14:40 +0700

    lala

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,10 @@ const pool = new Pool({
     port: 5432,
     database: 'dddc5slfjk0sj',
     user: 'lpaguthtnfozff',
-    password: '89393c66e789c2c7954efde235a9a64a8a9964fa21dde562a69417346d48c1f5'
+    password: '89393c66e789c2c7954efde235a9a64a8a9964fa21dde562a69417346d48c1f5',
+    connectionTimeoutMillis: 5000,
+    idleTimeoutMillis: 2000,
+    max: 200,
 });
 
 module.exports = pool;
