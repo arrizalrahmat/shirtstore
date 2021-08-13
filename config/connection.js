e8bd5518455d7e39c51da1a580a2d97373b42974
arrizalrahmat <arrizalrahmat@gmail.com>
2021-08-13 09:36:11 +0700

    final2

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    host: 'localhost',
+    host: 'ec2-34-194-14-176.compute-1.amazonaws.com',
     port: 5432,
-    database: 'shirtstore',
-    user: 'postgres',
-    password: 'l3montea',
+    database: 'dddc5slfjk0sj',
+    user: 'lpaguthtnfozff',
+    password: '89393c66e789c2c7954efde235a9a64a8a9964fa21dde562a69417346d48c1f5',
     connectionTimeoutMillis: 5000,
     idleTimeoutMillis: 2000,
     max: 200,
